c471ed214ba35bdde882dcec50222cc6d88825c7
insafe123 <83227525+insafe123@users.noreply.github.com>
2023-05-15 15:52:33 +0100

    frontend

--- src/components/upload.js
+++ src/components/upload.js
@@ -3,35 +3,73 @@ import axios from 'axios';
 
 function UploadComponent() {
   const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handleFileUpload = (event) => {
     setFile(event.target.files[0]);
   };
+const handleSubmit = async (event) => {
+  event.preventDefault();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const formData = new FormData();
+  formData.append('file', file);
+  setUploading(true);
+  setMessage('');
+  try {
+    const response = await axios.post('http://127.0.0.1:8000/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    console.log('Model predicted:', response.data.fileName);
+    setMessage(`Model predicted`);
+    // Access the predictions data from the response
+    const predictions = response.data.predictions;
+    console.log('Predictions:', predictions);
+  } catch (error) {
+    console.error('Error Predicting file:', error);
+    setMessage('Error Predicting file');}
+      finally{
+      setUploading(false);
+   }
 
-    const formData = new FormData();
-    formData.append('file', file);
+};
 
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      console.log('Model uploaded:', response.data.fileName);
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
-  };
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //
+  //   const formData = new FormData();
+  //   formData.append('file', file);
+  //
+  //   setUploading(true);
+  //   setMessage('');
+  //
+  //   try {
+  //     const response = await axios.post('http://127.0.0.1:8000/upload', formData, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data',
+  //       },
+  //     });
+  //     console.log('Model uploaded:', response.data.fileName);
+  //     setMessage(`Model uploaded`);
+  //   } catch (error) {
+  //     console.error('Error Predicting file:', error);
+  //     setMessage('Error Predicting file');
+  //   } finally {
+  //     setUploading(false);
+  //   }
+  // };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleFileUpload} />
-        <button type="submit">Predict</button>
+        <button type="submit" disabled={uploading}>
+          {uploading ? 'Predicting...' : 'Predict'}
+        </button>
       </form>
+      {message && <div>{message}</div>}
     </div>
   );
 }
